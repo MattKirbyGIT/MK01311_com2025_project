79a7da08b2b20543af6c1c83e3e339d0888ed68d
MattKirbyGIT <mattjkirby@outlook.com>
2020-11-30 01:54:11 +0000

    Added contact page and contact controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'mybookings', to: 'my_bookings#index'
   get 'mybookings/find', to: "my_bookings#find"
   post 'book', to: "bookings#parseAjax"
+  get 'contact', to: "home#contact"
+  post 'request_contact', to: 'home#request_contact'
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,26 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    
+    assert_select 'h1','Contact Us'
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do
+    post request_contact_url, params:{name: "John", email: "JohnSmith@gmail.com",telephone: "1234567890", message: "This is a test message"}
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
+
+
 end
