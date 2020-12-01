516d572f04775c52ab97b061e60b34ed2fb6af85
MattKirbyGIT <mattjkirby@outlook.com>
2020-12-01 01:32:26 +0000

    showings model tests

--- test/models/showing_test.rb
+++ test/models/showing_test.rb
@@ -7,4 +7,22 @@ class ShowingTest < ActiveSupport::TestCase
     showing.save
     refute showing.valid?
   end
+
+  test 'should save valid showing' do
+    showing = Showing.new
+    
+    showing.film = films(:one);
+    showing.venue = venues(:one);
+
+    showing.time = "11:00:28"
+    showing.date = 2020-11-18
+    showing.price = 9.90
+    showing.save
+
+    assert showing.valid?
+    assert_match showing.film.title, "MyString 1"
+    assert_match showing.venue.name, "MyString"
+  end
+
+
 end
