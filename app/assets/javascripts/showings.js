81790145546b82fe8f671efbadae3f559bcf427e
MattKirbyGIT <mattjkirby@outlook.com>
2020-12-05 23:59:17 +0000

    seat popout

--- app/assets/javascripts/showings.js
+++ app/assets/javascripts/showings.js
@@ -1,3 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+popoutToggle = (id,popout) =>{
+    var cell_id = id.replace(/\D/g,'');
+    if(popout){
+        $("#popout-" + cell_id).fadeIn(200);
+   
+    } else {
+        
+        $("#popout-" + cell_id).fadeOut(200);
+    }
+}
