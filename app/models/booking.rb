024f390f89754aa6641c565b5a0079858fc8db87
MattKirbyGIT <mattjkirby@outlook.com>
2020-12-13 01:54:13 +0000

    fixed booking model validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,6 +6,6 @@ class Booking < ApplicationRecord
   validates :email, presence: true
   validates :seat, presence: true, inclusion: {in: 1..50}
   validates :row, presence: true,  inclusion: {in: 1..50}
-  # E_ticket validates so that it must be unique
-  validates :E_ticket, presence: true, uniqueness: true
+  # E_ticket cant be unique since each booking only contains only one seat and will need same e ticket number as other bookings when multiple seats are selected.  
+  validates :E_ticket, presence: true
 end
